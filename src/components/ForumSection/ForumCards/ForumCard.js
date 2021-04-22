c69a6e408bbad14946793568f77d5a1b5789e4b4
bagusaff <fkrfajri@gmail.com>
2021-04-22 12:57:36 +0700

    removing avatar

--- src/components/ForumSection/ForumCards/ForumCard.js
+++ src/components/ForumSection/ForumCards/ForumCard.js
@@ -1,40 +1,67 @@
-import React from 'react'
-import { Avatar, Bio, CardContainer, Header, TimeStamp, UserWrapper, Wrapper,Content,Footer, ForumLink, CommentLink } from './ForumCardElement'
-import {Hidden} from 'react-grid-system'
-import {FaRegCommentDots} from 'react-icons/fa'
+import React from "react";
+import {
+  Avatar,
+  Bio,
+  CardContainer,
+  Header,
+  TimeStamp,
+  UserWrapper,
+  Wrapper,
+  Content,
+  Footer,
+  ForumLink,
+  CommentLink,
+} from "./ForumCardElement";
+import { Hidden } from "react-grid-system";
+import { FaRegCommentDots } from "react-icons/fa";
 
-import avatar from '../../../images/Avatar.png'
-const ForumCard = ({idx,author,date_post,title,content,url,job,comment}) => {
-    return (
-        <>
-            <CardContainer>
-                <Wrapper>
-                    <Header>
-                        <UserWrapper>
-                            <Avatar src={avatar}/>
-                            <Bio>
-                                <h4>{author}</h4>
-                                <h5>{job}</h5>
-                            </Bio>
-                        </UserWrapper>
-                        <TimeStamp>
-                            <h4>{date_post}</h4>
-                        </TimeStamp>
-                    </Header>
-                    <Content>
-                        <h3>{title}</h3>
-                        <p>{content}</p>
-                    </Content>
-                    <Footer>
-                        <CommentLink href={url}>
-                            <FaRegCommentDots/><span>{comment} <Hidden xs sm>Komentar</Hidden></span>
-                        </CommentLink>
-                        <ForumLink href={url}>Lihat Detail Diskusi</ForumLink>
-                    </Footer>
-                </Wrapper>
-            </CardContainer>
-        </>
-    )
-}
+// import avatar from '../../../images/Avatar.png'
+const ForumCard = ({
+  idx,
+  author,
+  date_post,
+  title,
+  content,
+  url,
+  job,
+  comment,
+}) => {
+  return (
+    <>
+      <CardContainer>
+        <Wrapper>
+          <Header>
+            <UserWrapper>
+              {/* <Avatar src={avatar}/> */}
+              <Bio>
+                <h4>{author}</h4>
+                <h5>{job}</h5>
+              </Bio>
+            </UserWrapper>
+            <TimeStamp>
+              <h4>{date_post}</h4>
+            </TimeStamp>
+          </Header>
+          <Content>
+            <h3>{title}</h3>
+            <p>{content}</p>
+          </Content>
+          <Footer>
+            <CommentLink href={url}>
+              <FaRegCommentDots />
+              <span>
+                {comment}{" "}
+                <Hidden xs sm>
+                  Komentar
+                </Hidden>
+              </span>
+            </CommentLink>
+            <ForumLink href={url}>Lihat Detail Diskusi</ForumLink>
+          </Footer>
+        </Wrapper>
+      </CardContainer>
+    </>
+  );
+};
 
-export default ForumCard
+export default ForumCard;
--- src/components/ForumSection/SiderCards/SiderPost.js
+++ src/components/ForumSection/SiderCards/SiderPost.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import { PostWrapper, TextInnerWrapper, TextWrapper, Avatar } from './SiderCardElement'
+import React from "react";
+import {
+  PostWrapper,
+  TextInnerWrapper,
+  TextWrapper,
+  Avatar,
+} from "./SiderCardElement";
 
-import avatar from '../../../images/Avatar-2.png'
+// import avatar from '../../../images/Avatar-2.png'
 
-const SiderPost = ({author,date_post,title}) => {
-    return (
-        <>
-            <PostWrapper className="borders">
-                <Avatar src={avatar}/>
-                <TextWrapper>
-                    <TextInnerWrapper>
-                        <h4>{author}</h4>
-                        <h5>{date_post}</h5>
-                    </TextInnerWrapper>
-                    <p>Mengajukan Proposal <strong>“{title}”</strong></p>
-                </TextWrapper>
-            </PostWrapper>
-        </>
-    )
-}
+const SiderPost = ({ author, date_post, title }) => {
+  return (
+    <>
+      <PostWrapper className="borders">
+        {/* <Avatar src={avatar}/> */}
+        <TextWrapper>
+          <TextInnerWrapper>
+            <h4>{author}</h4>
+            <h5>{date_post}</h5>
+          </TextInnerWrapper>
+          <p>
+            Mengajukan Proposal <strong>“{title}”</strong>
+          </p>
+        </TextWrapper>
+      </PostWrapper>
+    </>
+  );
+};
 
-export default SiderPost
+export default SiderPost;
